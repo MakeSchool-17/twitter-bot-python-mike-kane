1405b2eed4d732de98b6d8189edc5395320af13f
Mike Kane <mike.kane@students.makeschool.com>
2015-09-29 14:45:30 -0700

    Initial commit

---
+++ linked_list_practice.py
@@ -0,0 +1,10 @@
+import LinkedList
+
+myList = LinkedList()
+
+myList.append('a')
+myList.append('b')
+myList.append('c')
+
+print('head: ' + str(myList.head.data))
+print('tail: ' + str(myList.tail.data))
